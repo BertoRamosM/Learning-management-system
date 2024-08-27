061ed09218d1b5693b4c9aec671e191be74280bd
ARamos <albertramos902@gmail.com>
2024-08-27 17:52:44 +0000

    working on structure

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <p className="text-red-500 font-semibold text-4xl">hello world</p>
+      <Button variant="destructive">
+        Click me
+      </Button>
+</div>
+  );
+}
--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-
-export default function Home() {
-  return (
-    <p className="text-red-500 font-semibold text-4xl">hello world</p>
-  );
-}
