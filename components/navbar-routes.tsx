e5085910c81501fb5365ead33b13141937a969a7
ARamos <albertramos902@gmail.com>
2024-08-28 09:28:16 +0000

    navbar routes depending on user role

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,14 +1,37 @@
 'use client'
 import { UserButton } from '@clerk/nextjs'
+import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
+import { Button } from './ui/button'
+import {  LogOut } from 'lucide-react'
+import Link from 'next/link'
 
 
 const NavbarRoutes = () => {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  const isTeacherPage = pathname?.startsWith("/teacher")
+  const isPlayerPage = pathname?.includes("/chapter")
+
   return (
-    <div className='flex gap-x-2 ml-auto'>
-      <UserButton />
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton 
+      />
     </div>
-  )
+  );
 }
 
 export default NavbarRoutes
