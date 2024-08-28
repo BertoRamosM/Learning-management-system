41363c59cd37fa1e321698c87ec167f3f13f560e
ARamos <albertramos902@gmail.com>
2024-08-28 16:40:13 +0000

    working on add course form

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const CoursesPage = () => {
   return (
-    <div>CoursesPage</div>
-  )
+    <div>
+      <Link href="/teacher/create">
+        <Button className="p-6">New Course</Button>
+      </Link>
+    </div>
+  );
 }
 
 export default CoursesPage
