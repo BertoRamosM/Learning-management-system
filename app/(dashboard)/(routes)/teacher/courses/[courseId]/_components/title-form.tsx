7094a64c60c49778128d8816810d816dfa44efe5
ARamos <albertramos902@gmail.com>
2024-09-01 14:01:25 +0000

    finished form to update titles

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -1,58 +1,61 @@
-"use client"
-import * as z from "zod"
-import axios from "axios"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+"use client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormMessage
-} from "@/components/ui/form"
+  FormMessage,
+} from "@/components/ui/form";
 
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Pencil } from "lucide-react"
-import { useState } from "react"
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
 
-interface TitleFormProps{
+interface TitleFormProps {
   initialData: {
-    title: string, 
-  },
-  courseId: string
+    title: string;
+  };
+  courseId: string;
 }
 
 const formSchema = z.object({
   title: z.string().min(1, {
-    message: "Title is required"
-  })
-})
+    message: "Title is required",
+  }),
+});
 
 export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-  const [isEditing, setIsEditing] = useState(false)
-  
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: initialData
-  })
+    defaultValues: initialData,
+  });
 
-  const {isSubmitting, isValid} = form.formState
+  const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-  }
+    console.log(values);
+  };
 
+  const toggleEdit = () => {
+    setIsEditing((current) => !current);
+  };
 
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
         Course title
-        <Button variant="ghost">
-          {isEditing && <>Cancel</>}
-          {!isEditing && (
+        <Button variant="ghost" onClick={toggleEdit}>
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
             <>
               <Pencil className="h-4 w-4 mr-2" />
               Edit title
@@ -60,6 +63,36 @@ export const TitleForm = ({ initialData, courseId }: TitleFormProps) => {
           )}
         </Button>
       </div>
+      {!isEditing && (
+        <p className="text-sm mt-2">{initialData.title}</p>
+      )}
+
+      {isEditing && (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 mt-4">
+          <FormField control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    disabled={isSubmitting}
+                    placeholder="e.g. 'Advanced web development"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting} type="submit">
+                Save
+                </Button>
+            </div>
+        </form>
+        </Form>
+      )}
     </div>
   );
-}
+};
