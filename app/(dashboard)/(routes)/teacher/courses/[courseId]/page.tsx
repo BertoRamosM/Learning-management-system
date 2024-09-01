bd9db4ffc1cd90712d1838225eb181fd2ebf5888
ARamos <albertramos902@gmail.com>
2024-09-01 16:56:32 +0000

    created descriptuon form field

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { db } from '@/lib/db'
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation'
-import { IconBadge } from '@/components/icon-badge'
-import { LayoutDashboard } from 'lucide-react'
-import { TitleForm } from './_components/title-form'
+import React from "react";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { IconBadge } from "@/components/icon-badge";
+import { LayoutDashboard } from "lucide-react";
+import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form.tsx";
 
-const CourseIdPage = async({
-  params
+const CourseIdPage = async ({
+  params,
 }: {
-    params: {
-      courseId: string
-    }
-  }) => {
-  
-  const { userId } = auth()
-  
+  params: {
+    courseId: string;
+  };
+}) => {
+  const { userId } = auth();
+
   if (!userId) {
-    return redirect("/")
+    return redirect("/");
   }
 
   const course = await db.course.findUnique({
     where: {
-      id: params.courseId
-    }
-  })
+      id: params.courseId,
+    },
+  });
 
   if (!course) {
-    return redirect("/")
+    return redirect("/");
   }
 
   const requiredFields = [
@@ -35,21 +35,23 @@ const CourseIdPage = async({
     course.description,
     course.imageUrl,
     course.price,
-    course.categoryId
-  ]
+    course.categoryId,
+  ];
 
-  const totalFields =  requiredFields.length
-  const completedFields = requiredFields.filter(Boolean).length
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
 
-  const completionText = `(${completedFields}/${totalFields})`
+  const completionText = `(${completedFields}/${totalFields})`;
 
   return (
-    <div className='p-6'>
+    <div className="p-6">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-y-2">
-          <h1 className='text-2xl font-medium'>Course setup</h1>
+          <h1 className="text-2xl font-medium">Course setup</h1>
 
-          <span className='text-sm text-slate-700'>Complete all fields {completionText}</span>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
@@ -58,14 +60,12 @@ const CourseIdPage = async({
             <IconBadge icon={LayoutDashboard} />
             <h2 className="text-xl">Customize your course</h2>
           </div>
-          <TitleForm
-            initialData={course}
-            courseId={course.id}
-          />
+          <TitleForm initialData={course} courseId={course.id} />
+          <DescriptionForm initialData={course} courseId={course.id} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CourseIdPage
+export default CourseIdPage;
