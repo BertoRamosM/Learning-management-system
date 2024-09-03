dca8fc0452605eb607645b782b2f63df2072e800
ARamos <albertramos902@gmail.com>
2024-09-03 17:38:34 +0000

    working on the dragable list

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -3,6 +3,15 @@
 import { Chapter } from "@prisma/client"
 import { useEffect, useState } from "react"
 
+import { 
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult
+} from "@hello-pangea/dnd"
+
+import { cn } from "@/lib/utils"
+
 interface ChaptersListProps {
   items: Chapter[]
   onEdit: (id: string) => void
@@ -27,8 +36,33 @@ export const ChaptersList = ({items, onEdit, onReorder} : ChaptersListProps) => 
 
 
   return (
-    <div>
-      
-    </div>
+    <DragDropContext
+      onDragEnd={()=>{})}>
+        <Droppable droppableId="chapters">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {chapters.map((chapter, index) => (
+                <Draggable key={chapter.id} draggableId={chapter.id} index={index}>
+                  {(provided) => (
+                    <div
+                      className={cn(
+                        "flex items-center gap-x-2 bg-slate-200 border-slate-200 border text-slate-700 rounded-md mb-4 text-sm",
+                        chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                      )}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <ChapterItem chapter={chapter} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+
+            </div>
+          )}
+        </Droppable>
+
+    </DragDropContext>
   )
 }
