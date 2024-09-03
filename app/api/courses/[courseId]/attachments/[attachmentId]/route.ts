88db174023b34de0a48d4afa18267873812afb08
ARamos <albertramos902@gmail.com>
2024-09-03 13:20:14 +0000

    fixed delete attachment bug

--- app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -1,13 +1,16 @@
-import { db } from "@/lib/db"
-import { auth } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server"
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { courseId: string, attachmentId: string } }) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; attachmentId: string } }
+) {
   try {
-    const { userId } = auth()
-    
+    const { userId } = auth();
+
     if (!userId) {
-      return new NextResponse("Unautherized", {status: 401})
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
     const courseOwner = await db.course.findUnique({
@@ -15,23 +18,31 @@ export async function DELETE(req: Request, { params }: { params: { courseId: str
         id: params.courseId,
         userId,
       },
-    })
+    });
 
-    if(!courseOwner) {
-      return new NextResponse("Unautherized", {status: 401})
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    const attachment = await db.attachment.delete({
+    const attachment = await db.attachment.findUnique({
       where: {
-        courseId: params.attachmentId,
         id: params.attachmentId,
-      }
-    })
+      },
+    });
 
-    return NextResponse.json(attachment)
-    
-  }catch (error) {
-    console.log("[ATTACHMENTS]", error)
-    return new NextResponse("Internal Error", {status: 500})
+    if (!attachment || attachment.courseId !== params.courseId) {
+      return new NextResponse("Attachment not found", { status: 404 });
+    }
+
+    await db.attachment.delete({
+      where: {
+        id: params.attachmentId,
+      },
+    });
+
+    return NextResponse.json({ message: "Attachment deleted successfully" });
+  } catch (error) {
+    console.log("[ATTACHMENTS]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
