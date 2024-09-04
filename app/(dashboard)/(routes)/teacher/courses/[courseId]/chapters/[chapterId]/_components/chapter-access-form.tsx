1c556a3f6621fe3dfbcb5871561eb22c0a23f120
ARamos <albertramos902@gmail.com>
2024-09-04 14:56:56 +0000

    functionalty to toggle free chapter preview completed

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-access-form.tsx
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormMessage,
@@ -20,6 +21,7 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import { Chapter } from "@prisma/client";
 import { Checkbox } from "@/components/ui/checkbox";
+import { cn } from "@/lib/utils";
 
 interface ChapterAccessFormProps {
   initialData: Chapter,
@@ -71,20 +73,24 @@ export const ChapterAccessForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Chapter title
+        Chapter access
         <Button variant="ghost" onClick={toggleEdit}>
           {isEditing ? (
             <>Cancel</>
           ) : (
             <>
               <Pencil className="h-4 w-4 mr-2" />
-              Edit title
+              Edit access
             </>
           )}
         </Button>
       </div>
       {!isEditing && (
-        <p className="text-sm mt-2">{initialData.title}</p>
+        <div className={cn("text-sm mt-2", 
+          !initialData.isFree && "text-slate-500 italic"
+        )}>
+          {initialData.isFree ? "This chapter is free for preview" : "This chapter is not free for preview"}
+        </div>
       )}
 
       {isEditing && (
@@ -93,10 +99,17 @@ export const ChapterAccessForm = ({
           <FormField control={form.control}
             name="isFree"
             render={({ field }) => (
-              <FormItem className="flex flex-row items-start gap-x-3 space-x-0 rounded-md border p-4">
-                <FormControl className="flex-1">
-                  <Checkbox />
+              <FormItem className="flex flex-row items-center gap-x-3 space-x-0 rounded-md border p-4">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value} onCheckedChange={field.onChange} />
                 </FormControl>
+
+                <div className="space-y-1 leading-none">
+                  <FormDescription>
+                    Check this box if box if you want to make this chapter free to preview.
+                  </FormDescription>
+                </div>
 
               </FormItem>
             )}
