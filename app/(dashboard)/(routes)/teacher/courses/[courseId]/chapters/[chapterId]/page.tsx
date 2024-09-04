21f4e0cce5dfff07ccfb146b155410cc751701b8
ARamos <albertramos902@gmail.com>
2024-09-04 15:07:00 +0000

    added the chapter video form component to the page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form.tsx";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
   params,
@@ -102,6 +103,7 @@ const ChapterIdPage = async ({
             <IconBadge icon={Video} />
             <h2 className="text-xl">Add a Video</h2>
           </div>
+          <ChapterVideoForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
         </div>
       </div>
     </div>
