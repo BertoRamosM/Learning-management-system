898e4bc951c9a1010c71a0ceb4679744ef6a76f9
ARamos <albertramos902@gmail.com>
2024-09-04 12:47:26 +0000

    copied and adapted description form to chapter

--- components/preview.tsx
+++ components/preview.tsx
@@ -8,7 +8,7 @@ interface PreviewProps {
   value: string;
 }
 
-export const Preview = ({ onChange, value }: PreviewProps) => {
+export const Preview = ({ value }: PreviewProps) => {
   const ReactQuill = useMemo(
     () => dynamic(() => import("react-quill"), { ssr: false }),
     []
