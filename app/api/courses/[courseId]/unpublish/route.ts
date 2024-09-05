9a7dd5c261808be8bf8cd902273db73a60150ece
ARamos <albertramos902@gmail.com>
2024-09-05 17:51:58 +0000

    copied publish course and adapted for unpublish

---
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const UnPublishedCourse = await db.course.update({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    return NextResponse.json(UnPublishedCourse);
+  } catch (error) {
+    console.log("[COURSE_PUBLISH_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
