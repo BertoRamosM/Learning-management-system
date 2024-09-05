6fe6889485677025b34d77c6c0c4e6d4adab9544
ARamos <albertramos902@gmail.com>
2024-09-05 17:23:30 +0000

    copied publish route and adapt it to create upublish feature

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -33,16 +33,14 @@ const ChapterActions = ({
           `/api/courses/${courseId}/chapters/${chapterId}/unpublish`
         );
         toast.success("Chapter unpublished");
-        router.refresh();
       } else {
         await axios.patch(
           `/api/courses/${courseId}/chapters/${chapterId}/publish`
         );
         toast.success("Chapter published");
-        router.refresh();
       }
 
-      router.push(`/teacher/courses/${courseId}`);
+      router.refresh();
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
