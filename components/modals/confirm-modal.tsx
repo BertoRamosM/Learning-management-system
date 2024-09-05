e6b10ccf5886df5edecfdd37f04b2271490eac0f
ARamos <albertramos902@gmail.com>
2024-09-05 13:18:57 +0000

    installed shadcn dialog alert and created modal component

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "../ui/alert-dialog"
+ 
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+
+export const ConfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <span>{children}</span>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+        </AlertDialogHeader>
+
+        <AlertDialogDescription>
+          This action cannot be undone.
+        </AlertDialogDescription>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={onConfirm}>Confirm</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+      </AlertDialog>
+      )
+    
+    }
