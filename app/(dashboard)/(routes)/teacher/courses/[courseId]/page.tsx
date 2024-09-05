2604cbf585abaec5cc75fd2d6367116c15b0d368
ARamos <albertramos902@gmail.com>
2024-09-05 17:34:21 +0000

    copied and adapted publish actions for courses

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -17,6 +17,7 @@ import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
 import { Banner } from "@/components/banner";
+import Actions from "./_components/actions";
 
 const CourseIdPage = async ({
   params,
@@ -94,6 +95,11 @@ const CourseIdPage = async ({
               Complete all fields {completionText}
             </span>
           </div>
+          <Actions
+            disabled={!isComplete}
+            courseId={params.courseId}
+            isPublished={course.isPublished}
+          />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
           <div>
