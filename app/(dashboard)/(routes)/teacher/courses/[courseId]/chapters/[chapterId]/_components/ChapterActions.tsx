762be8cf06a715d35cae4e74cacb89c539dc2f1d
ARamos <albertramos902@gmail.com>
2024-09-05 13:05:36 +0000

    created buttons to publish/unpublish and delete chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
 
 interface ChapterActionsProps {
   disabled: boolean;
@@ -17,7 +20,14 @@ const ChapterActions = ({
 }: ChapterActionsProps) => {
 
   return (
-    <div className=""></div>
+    <div className="flex items-center gap-x-2">
+      <Button onClick={() => { }} disabled={disabled} variant="outline" size="sm">
+        {isPublished ? "Unpublish" : "Publish"}</Button>
+      
+      <Button size="sm">
+        <Trash className="h-4 w-4" />
+      </Button>
+    </div>
   )
 }
 
