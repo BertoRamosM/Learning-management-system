8dd4f25b30f6a2f6335649241d32127a0f043256
ARamos <albertramos902@gmail.com>
2024-09-05 13:25:55 +0000

    created action to delete the chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/ChapterActions.tsx
@@ -1,7 +1,12 @@
 'use client'
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 
 interface ChapterActionsProps {
@@ -18,17 +23,43 @@ const ChapterActions = ({
   chapterId,
   isPublished
 }: ChapterActionsProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true)
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`, {
+        method: "DELETE",
+      });
+      toast.success("Chapter deleted");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      toast.error("Something went wrong")
+    } finally {
+      setIsLoading(false)
+     }
+  }
 
   return (
     <div className="flex items-center gap-x-2">
-      <Button onClick={() => { }} disabled={disabled} variant="outline" size="sm">
-        {isPublished ? "Unpublish" : "Publish"}</Button>
-      
-      <Button size="sm">
-        <Trash className="h-4 w-4" />
+      <Button
+        onClick={() => {}}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
       </Button>
+
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading} variant="outline">
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
     </div>
-  )
+  );
 }
 
 export default ChapterActions
