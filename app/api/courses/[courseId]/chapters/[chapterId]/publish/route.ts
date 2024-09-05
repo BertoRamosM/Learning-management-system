c83bc8840796e9cb98d53665b2f96a50423246c6
ARamos <albertramos902@gmail.com>
2024-09-05 14:06:46 +0000

    finished toggle published route

--- app/api/courses/[courseId]/chapters/[chapterId]/publish/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/publish/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
 
 
 export async function PATCH(
@@ -9,8 +10,52 @@ export async function PATCH(
 ) {
 
   try {
-    const {userId} = auth()
+    const { userId } = auth()
     
+    if(!userId) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    })
+
+    if(!ownCourse) {
+      return new NextResponse("Unauthorized", { status: 401 })
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      }
+    })
+
+    const muxData = await db.muxData.findUnique({
+      where: {
+        chapterId: params.chapterId,
+      }
+    })
+
+    if(!chapter || !muxData || !chapter.title || !chapter.description || !chapter.videoUrl) {
+      return new NextResponse("Missing requiered fields", { status: 404 })
+    }
+    
+    const publishedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        isPublished: true,
+      },
+    })
+
+    return NextResponse.json(publishedChapter)
+
   }catch (error) {
     console.log("[CHAPTERS_PUBLISH_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
