5e2b9dce62ef02cc730e2915c81aa556b2d0d3f5
ARamos <albertramos902@gmail.com>
2024-09-06 16:02:08 +0000

    to avoid unnecesary searchs on the database, using debounder on the input search

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,19 +1,45 @@
-'use client'
+"use client";
 
-import { Search } from 'lucide-react'
-import React from 'react'
-import { Input } from './ui/input'
+import { Search } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { useDebounce } from "@/hooks/use-debounce";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import qs from "query-string";
 
 const SearchInput = () => {
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value, 500);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const currentCategoryId = searchParams.get("categoryId");
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          title: debouncedValue,
+          categoryId: currentCategoryId,
+        },
+      },
+      { skipNull: true, skipEmptyString: true }
+    );
+    router.push(url);
+  }, [debouncedValue, currentCategoryId, pathname, router, searchParams]);
+
   return (
-    <div className='relative'>
-      <Search className='w-4 h-4 absolute left-3 top-3 text-slate-600'/>
-      <Input className='w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200'
-        placeholder='Search for a course'
+    <div className="relative">
+      <Search className="w-4 h-4 absolute left-3 top-3 text-slate-600" />
+      <Input
+        className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+        placeholder="Search for a course"
       />
-
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
