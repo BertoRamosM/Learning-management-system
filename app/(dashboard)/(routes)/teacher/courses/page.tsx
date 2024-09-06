a0f6e13106947298e0c883264248f0143894cdc2
ARamos <albertramos902@gmail.com>
2024-09-06 08:49:26 +0000

    fetching the courses from db and passing as data for table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -3,26 +3,34 @@ import Link from 'next/link'
 import React from 'react'
 import { DataTable } from './_components/data-table';
 import { columns } from './_components/columns';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { db } from '@/lib/db';
 
-async function getData(): Promise<any[]> {
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ];
-}
+
+
 
 const CoursesPage = async () => {
-  const data = await getData();
 
-  
+  const {userId} = auth()
+  if(!userId) {
+    return redirect('/')
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId
+    }, 
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+
+
   return (
     <div>
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 }
