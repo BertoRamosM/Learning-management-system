e2bc79a08e1d016625eff36ebd6e6695956eaced
ARamos <albertramos902@gmail.com>
2024-09-06 08:10:07 +0000

    installed zustand and confetti

--- app/api/courses/[courseId]/unpublish/route.ts
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -36,7 +36,7 @@ export async function PATCH(
 
     return NextResponse.json(UnPublishedCourse);
   } catch (error) {
-    console.log("[COURSE_PUBLISH_ERROR]", error);
+    console.log("[COURSE_UNPUBLISH_ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
