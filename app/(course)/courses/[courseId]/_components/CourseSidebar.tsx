fdcb08b25baa5d8b8610a4283143b25b1c0cb653
ARamos <albertramos902@gmail.com>
2024-09-07 14:25:06 +0000

    fetching the course for the navbar

--- app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
+++ app/(course)/courses/[courseId]/_components/CourseSidebar.tsx
@@ -1,4 +1,7 @@
+import { db } from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
 import { Chapter, Course, UserProgress } from '@prisma/client'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 interface CourseSidebarProps {
@@ -10,7 +13,24 @@ interface CourseSidebarProps {
   progressCount: number
 }
 
-const CourseSidebar = ({course, progressCount} : CourseSidebarProps) => {
+const CourseSidebar = async ({ course, progressCount }: CourseSidebarProps) => {
+
+  
+  const { userId } = auth()
+  
+  if (!userId) {
+    return redirect('/')
+  } 
+
+  const purchase = await db.purchase.findUnique({
+    where: {
+      userId_courseId: {
+        userId,
+        courseId: course.id
+      }
+    }
+  })
+
   return (
     <div>
 
