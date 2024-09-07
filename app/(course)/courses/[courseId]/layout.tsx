0bfb22847981fcb063340ce1a356a47f2f9ac6ae
ARamos <albertramos902@gmail.com>
2024-09-07 13:51:25 +0000

    working on single course page and layout

---
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -0,0 +1,12 @@
+const CourseLayout = ({ children, params }: {
+  children: React.ReactNode;
+  params: {courseId: string}
+ }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default CourseLayout
