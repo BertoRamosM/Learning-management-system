44645f08e9b507bd235a38ac2a9572644175e6f1
ARamos <albertramos902@gmail.com>
2024-09-08 07:56:02 +0000

    creating info banners for chapters

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,3 +1,5 @@
+import { GetChapter } from '@/actions/get-chapter'
+import { Banner } from '@/components/banner'
 import { auth } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import React from 'react'
@@ -9,11 +11,37 @@ const ChapterIdPage = async ({params} : {params: {courseId: string, chapterId: s
     return redirect('/')
   }
 
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase
+   } = await GetChapter({
+    userId,
+    courseId: params.courseId,
+    chapterId: params.chapterId
+   })
+  
+  if(!chapter || !course) {
+    return redirect('/')
+  }
+
+  const isLocked = !chapter.isFree && !purchase
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted
+
   return (
     <div>
-      chapter
+      {userProgress?.isCompleted && (
+        <Banner label="You have completed this chapter" variant="success" />
+      )}
+      {isLocked && (
+        <Banner label="You need to purchase this course to watch this chapter" variant="warning" />
+      )}
     </div>
-  )
+  );
 }
 
 export default ChapterIdPage
